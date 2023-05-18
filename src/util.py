4e807b69ba664e8f426c5eb3b02866b9de02b02f
Gary Simmons <gary.simmons@colorado.edu>
2023-05-18 05:05:25 -0700

    print out results

--- src/util.py
+++ src/util.py
@@ -11,3 +11,35 @@ def tryParse(x):
     except ValueError:
         isnumber = False
         return isnumber,numberValue
+
+def conversionPerUser(df):
+    uniqueUsers = list(set(df['source_visitor_id']))
+    conversionDictionary = {
+        'user': [],
+        'experimentalVisits': [],
+        'ordersMade': [],
+        'totalRevenueFromUser':[]
+    }
+    for user in uniqueUsers:
+        conversionDictionary['user'].append(user)
+        userDF = df[df['source_visitor_id'] == user]
+        conversionDictionary['experimentalVisits'].append(userDF.shape[0])
+        conversionDictionary['ordersMade'].append(userDF['purchase_flag'].sum())
+        conversionDictionary['totalRevenueFromUser'].append(userDF['orderRevenue'].sum())
+    conversionDF = pd.DataFrame(conversionDictionary,columns=list(conversionDictionary.keys()))
+    conversionDF['conversionRate'] = conversionDF['ordersMade']/conversionDF['experimentalVisits']
+    return conversionDF
+
+def displayFeatureImportances(columns,fittedModel):
+    importance = fittedModel.feature_importances_
+    featureImportance = pd.DataFrame({
+        "feature": columns,
+        "featureImportance": importance
+        },columns = ["feature","featureImportance"])
+    featureImportancesSorted = featureImportance.sort_values(by="featureImportance", ascending=False)
+    for i in range(10):
+        featureRow = featureImportancesSorted.iloc[i]
+        feature = featureRow['feature']
+        featureValue = featureRow['featureImportance']
+        print(f'Rank {i}: {feature}: score: {featureValue}')
+        
